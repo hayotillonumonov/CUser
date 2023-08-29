e2c94d5cbfc5c127271a31fa7656b782a49790b3
Hayotillo <hayotnumon@gmail.com>
2023-08-29 11:19:11 +0500

    Vite amaliyot

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+// Styles
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container conteiner">
+        <p>
+          All Right Reserved. ||
+          <a href="https://www.youtube.com/@hayotillonumonov" target="_blank"> Hayotillo Numonov</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/users/UserList.jsx
@@ -0,0 +1,28 @@
+// Styles
+import "./UserList.css";
+
+
+function UserList({ users, deleteUser }) {
+  return (
+    <div className="userList">
+      <div className="userList-container container">
+        {users.map((user) => {
+          return (
+            <div className="card" key={user.id}>
+              <div className="card-inner">
+                <img src={user.img} alt={user.name} width={150} height={150} />
+                <h3> {user.firstName} {user.className}, {user.age} age </h3>
+                <p>From: {user.from}</p>
+                <p>Job:{user.job}</p>
+                <p>Gender: {user.gender}</p>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default UserList;
